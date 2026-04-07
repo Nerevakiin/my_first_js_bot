d383b080d11c4842b90e48ee61dcd156cd58a384
Nerevakiin <djmatisk@yahoo.gr>
2026-04-07 21:28:46 +0300

    added menu btns

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Telegraf } from 'telegraf'
+import { Markup } from 'telegraf'
 import 'dotenv/config'
 
 // 1. initialize the bot
@@ -17,6 +18,20 @@ bot.hears('hello there', (ctx) => ctx.reply('General Kenobi!'))
 
 //5. handling stickers
 bot.on('sticker', (ctx) => ctx.reply('oreo kapelo aderfe'))
+
+// test markup buttons
+bot.command('menu', (ctx) => {
+    return ctx.reply('ti einai xeirotero?',
+        Markup.inlineKeyboard([
+            [Markup.button.callback('na se dei o mpampas sou na ton pairneis?', 'opt_1'), 
+                Markup.button.callback('na deis ton mpampa sou na ton pairnei', 'opt_2')],
+            [Markup.button.url('bes edw na deis mia ekpliksi', 'https://www.youtube.com/watch?v=b7VhXzcacU4')]
+        ])
+    )
+})
+
+// handle the button click
+bot.action('opt_1', (ctx) => ctx.answerCbQuery('patises to 1'))
 
 // 6. launch the bot
 bot.launch()
