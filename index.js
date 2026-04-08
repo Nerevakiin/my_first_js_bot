82419d17152f97ac91d102d1744eb23e026c4913
Nerevakiin <djmatisk@yahoo.gr>
2026-04-08 20:41:23 +0300

    development

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ import 'dotenv/config'
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 // 2. command /start
-bot.start((ctx) => {
-    ctx.reply(`Welcome ${ctx.from.first_name}! I am your new node.js bot`)
-})
+// bot.start((ctx) => {
+//     ctx.reply(`Welcome ${ctx.from.first_name}! I am your new node.js bot`)
+// })
 
 // 3. command /help
 bot.help((ctx) => ctx.reply(`Send me a sticker or just say hi!!`))
@@ -32,6 +32,76 @@ bot.command('menu', (ctx) => {
 
 // handle the button click
 bot.action('opt_1', (ctx) => ctx.answerCbQuery('patises to 1'))
+bot.action('opt_2', (ctx) => ctx.answerCbQuery('patises to 2'))
+
+
+
+
+// =======================================
+
+// 1. MAIN MENU
+const mainMenu = (ctx) => {
+    return ctx.reply('Ti tha itheles na kaneis?',
+        Markup.inlineKeyboard([
+            [
+                Markup.button.callback('📝 Ftiakse ena task', 'create_task'),
+                Markup.button.callback('📂 Des ta tasks sou', 'view_tasks')
+            ],
+            [Markup.button.callback('⚙️ Rithmiseis', 'settings')]
+        ])
+    )
+}
+
+
+// ============ START THE BOT ================
+bot.start((ctx, mainMenu) => {
+    ctx.reply(`Welcome ${ctx.from.first_name}! I am your new node.js bot`)
+    mainMenu(ctx)
+})
+
+// 2. HANDLING BUTTON CLICKS (actions)
+
+bot.action('create_task', async (ctx) => {
+    // answerCbQuery stops the 'loading' spinner on the button
+    await ctx.answerCbQuery()
+
+    // instead of a new message we EDIT the current one
+    await ctx.editMessageText('Grapse to onoma tou task sou...',
+        Markup.inlineKeyboard([
+            [Markup.button.callback('⬅️ Pisw sto menu', 'back_to_menu')]
+        ])
+    )
+})
+
+
+bot.action('view_tasks', async (ctx) => {
+    await ctx.answerCbQuery()
+    await ctx.editMessageText('Exeis 0 energa tasks',
+        [Markup.inlineKeyboard.callback('⬅️ Pisw sto menu', 'back_to_menu')]
+    )
+})
+
+
+// 3. THE "BACK" LOGIC
+bot.action('back_to_menu', async (ctx) => {
+    await ctx.answerCbQuery()
+    // Since mainMenu uses ctx.reply (new message), 
+    // we manually edit this one back to the start state.
+    await ctx.editMessageText('Ti tha itheles na kaneis?',
+        Markup.inlineKeyboard([
+            [
+                Markup.button.callback('📝 Ftiakse ena task', 'create_task'),
+                Markup.button.callback('📂 Des ta tasks sou', 'view_tasks')
+            ],
+            [Markup.button.callback('⚙️ Rithmiseis', 'settings')]
+        ])
+    )
+})
+
+
+
+
+
 
 // 6. launch the bot
 bot.launch()
